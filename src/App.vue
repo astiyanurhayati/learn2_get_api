10680c8242269afb51c19f188fb4d25d88da2c9c
astiyanurhayati <astiyanurhayati18@gmail.com>
2022-11-06 13:21:33 +0700

    try try

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderComponent />
+
+  <div class="container mx-auto">
+    <router-view />
+  </div>
+</template>
+
+<style>
+
+</style>
+<script>
+import HeaderComponent from '@/components/Header.vue'
+
+export default {
+  components: {
+    HeaderComponent,
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+ <header class="text-center bg-blue-800 text-white p-4 mb-10">
+  <div class="text-3xl mdd:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid 19 Tracker</div>
+
+  <p>API by <a class="text-blue-300" href="https://covid19api.com" target="_blank">Covid19api.com</a></p>
+ </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
